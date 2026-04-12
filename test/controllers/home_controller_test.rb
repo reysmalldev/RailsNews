b15406f39a2384228f4f4ff7815313009563dfee
Inácio <guisousa.inacio@gmail.com>
2026-04-12 14:34:42 -0300

    Fix: arruma url do action correto no arquivo teste

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get news_url
     assert_response :success
   end
 end
